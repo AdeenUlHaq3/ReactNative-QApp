8e5ebed82bac3359272a21fe89b7b80131ec6d47
adeenulhaq3 <adeen553@gmail.com>
2018-12-31 13:30:52 +0500

    Design Landing Page

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -2,9 +2,10 @@ import React from 'react';
 import {
   Button,
   StyleSheet,
-  View
+  View,
+  Text
 } from 'react-native';
-import { ListRow } from 'teaset';
+// import { ListRow } from 'teaset';
 
 export default class LoginScreen extends React.Component {
 
@@ -29,27 +30,36 @@ export default class LoginScreen extends React.Component {
 
   render() {
     return (
-      <View style={{ flex: 1 }}>
-        <View style={{ flex: 1, flexDirection: 'row' }}>
-          <View style={{ backgroundColor: '#ccc', flexDirection: 'column', flex: 0.2 }}></View>
-          {/* <Button
-          title='Login with FB'
-          onPress={this.login}
-        >
-        </Button> */}
-          {/* <ListRow
+      <View style={{ flex: 1, flexDirection: 'row' }}>
+        <View style={{ backgroundColor: '#ccc', flexDirection: 'column', flex: 0.2 }}></View>
+        {/* <ListRow
           title='Swipe able'
           swipeActions={[
             <ListRow.SwipeActionButton title='Cancel' />,
             <ListRow.SwipeActionButton title='Remove' type='danger' onPress={() => alert('Remove')} />,
           ]}
         /> */}
-        </View>
-        <View style={{ flex: 1, flexDirection: 'row' }}>
-          <View style={{ backgroundColor: '#ccc', flexDirection: 'row', flex: 0.4 }}></View>
-
+        <View style={{ flex: 0.8, flexDirection: 'column' }}>
+          <View style={{ backgroundColor: '#333', flexDirection: 'row', flex: 0.25 }}>
+            <Text>Logo</Text>
+          </View>
+          <View style={{ backgroundColor: '#123', flexDirection: 'row', flex: 0.25 }}>
+            <Button
+              title='Login with FB'
+              onPress={this.login}
+              style={{ flex: 1, flexDirection: 'row' }}
+            >
+            </Button>
+          </View>
+          <View style={{ backgroundColor: '#345', flexDirection: 'row', flex: 0.25 }}>
+            <Text>App Details</Text>
+          </View>
+          <View style={{ backgroundColor: '#166', flexDirection: 'row', flex: 0.25 }}>
+            <Text>EXIT</Text>
+          </View>
         </View>
       </View>
+
     );
   }
 
